0c2aba4386ad9d9129cafe1e1568359f787f1485
J. Santos <jamillo@gmail.com>
2018-03-14 17:12:51 -0300

    Adds the new method for creating Migration for registration: NewCodeMigration

--- migration_default.go
+++ migration_default.go
@@ -1,6 +1,12 @@
 package migration
 
-import "time"
+import (
+	"time"
+	"runtime"
+	"fmt"
+	"regexp"
+	"path"
+)
 
 type BaseMigration struct {
 	id          time.Time
@@ -8,7 +14,7 @@ type BaseMigration struct {
 }
 
 // GetID returns the ID of the migration.
-func (m *BaseMigration) GetID() time.Time{
+func (m *BaseMigration) GetID() time.Time {
 	return m.id
 }
 
@@ -55,6 +61,29 @@ func NewMigration(id time.Time, description string, handlers ...Handler) *Defaul
 		},
 		do:   do,
 		undo: undo,
+	}
+}
+
+var codeMigrationRegex = regexp.MustCompile("^([0-9]{4}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2})_(.*).go$")
+
+// NewCodeMigration uses the regex to extract data NewMigration.
+//
+// If a handler is provided it will assigned to the Up method. If a second is
+// provided, it will be assigned to the Down method.
+func NewCodeMigration(handlers ...Handler) *DefaultMigration {
+	_, file, _, ok := runtime.Caller(1)
+	if ok {
+		groups := codeMigrationRegex.FindStringSubmatch(path.Base(file))
+		if len(groups) == 3 {
+			id, err := time.Parse("20060102150405", groups[1])
+			if err != nil {
+				panic(fmt.Sprintf("the file name '%s' has an invalid datetime"))
+			}
+			return NewMigration(id, groups[2], handlers...)
+		}
+		panic(fmt.Sprintf("the file name '%s' has an invalid format"))
+	} else {
+		panic(fmt.Sprintf("the file name '%s' has an invalid format"))
 	}
 }
 
