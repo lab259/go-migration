30645c7627b56926eb0ac42f49d69c0b4a6a12c2
Felipe Pontes <felipemfpontes@gmail.com>
2019-07-16 10:07:20 -0300

    Migrate to Go Modules (#4)
    Fix CircleCI configuration
    Add CircleCI Cache
    Add CircleCI Test Summary

--- migration_test.go
+++ migration_test.go
@@ -1,13 +1,28 @@
 package migration_test
 
 import (
-	"github.com/jamillosantos/macchiato"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"os"
+	"path"
 	"testing"
+
+	"github.com/jamillosantos/macchiato"
+	"github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
+	. "github.com/onsi/gomega"
 )
 
 func TestMigration(t *testing.T) {
 	RegisterFailHandler(Fail)
-	macchiato.RunSpecs(t, "Migration Test Suite")
+	description := "Migration Test Suite"
+
+	if os.Getenv("CI") == "" {
+		macchiato.RunSpecs(t, description)
+	} else {
+		reporterOutputDir := "./test-results/go-migration"
+		os.MkdirAll(reporterOutputDir, os.ModePerm)
+		junitReporter := reporters.NewJUnitReporter(path.Join(reporterOutputDir, "results.xml"))
+		macchiatoReporter := macchiato.NewReporter()
+		ginkgo.RunSpecsWithCustomReporters(t, description, []ginkgo.Reporter{macchiatoReporter, junitReporter})
+	}
 }
